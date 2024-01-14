335f5ee2af825ab5c7c74ce1b5c88bb2ffc2a1a1
plzusername <MohamedFaris2210@gmail.com>
2024-01-14 18:17:37 +0300

    Add htmlWebpackPlugin options to Config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,19 @@
-let path=require('path')
+const path=require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
       filename: 'main.js',
       path: path.resolve(__dirname, 'dist'),
+      clean:true,
     },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'/src/index.html',
+            filename:'index.html',
+            title:'Mini Uncle Shawarma!'
+        })
+    ]
   };
   
