29d5b94ac71e272ec38c0d03ff69337d87662d25
plzusername <MohamedFaris2210@gmail.com>
2024-01-14 20:35:52 +0300

    Track dist directory,add html boilerplate for test

---
+++ dist/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('HI there')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsdUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9vZGluX3Jlc3RhdXJhbnRfcGFnZS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnSEkgdGhlcmUnKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
