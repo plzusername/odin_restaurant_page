be7a2779258e82248cc4449763486370df7c1900
plzusername <MohamedFaris2210@gmail.com>
2024-01-14 18:25:46 +0300

    Add asset management option

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-      filename: 'main.js',
+      filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
       clean:true,
     },
@@ -14,6 +14,22 @@ module.exports = {
             filename:'index.html',
             title:'Mini Uncle Shawarma!'
         })
-    ]
+    ],
+    module:{
+        rules:[
+            {
+                test: /\.css$/i,
+                use:['style-loader','css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    }
   };
   
