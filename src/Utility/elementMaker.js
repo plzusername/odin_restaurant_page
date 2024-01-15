d8f75e4550e160c0a4c94123f6440774d9fc8b7f
plzusername <MohamedFaris2210@gmail.com>
2024-01-15 15:12:53 +0300

    Create utility function to create elements

---
+++ src/Utility/elementMaker.js
@@ -0,0 +1,23 @@
+function elemCreator( element, parentElement = document.body , elementContent = ' '  , elementAttributes = {}){
+
+    let createdElement = document.createElement( element )
+  
+    for (const [key, value] of Object.entries(elementAttributes)) {
+        for (let i = 0 ; i < value.length ; i++) {
+  
+            const element = value[i];
+  
+            createdElement.setAttribute(key, element)
+            
+        }
+    }
+  
+    createdElement.innerHTML=elementContent
+  
+    parentElement.appendChild(createdElement) 
+  
+    return createdElement
+  
+}
+
+export {elemCreator}
