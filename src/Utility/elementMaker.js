658453ae695ebb35b293ddf922f6657f0224e6eb
plzusername <MohamedFaris2210@gmail.com>
2024-01-16 22:02:21 +0300

    Add fallbacks for edge cases of no inputed element

--- src/Utility/elementMaker.js
+++ src/Utility/elementMaker.js
@@ -2,23 +2,30 @@ const content=document.querySelector('.content')
 
 function elemCreator( element, parentElement = document.body , elementContent = ' '  , elementAttributes = {}){
 
-    let createdElement = document.createElement( element )
-  
-    for (const [key, value] of Object.entries(elementAttributes)) {
-        for (let i = 0 ; i < value.length ; i++) {
-  
-            const element = value[i];
-  
-            createdElement.setAttribute(key, element)
-            
+    if(element){
+
+        let createdElement = document.createElement( element )
+
+        for (const [key, value] of Object.entries(elementAttributes)) {
+            for (let i = 0 ; i < value.length ; i++) {
+      
+                const element = value[i];
+      
+                createdElement.setAttribute(key, element)
+                
+            }
         }
+      
+        createdElement.innerHTML=elementContent
+      
+        parentElement.appendChild(createdElement) 
+      
+        return createdElement
+    
+
     }
+
   
-    createdElement.innerHTML=elementContent
-  
-    parentElement.appendChild(createdElement) 
-  
-    return createdElement
   
 }
 
