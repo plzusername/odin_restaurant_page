dee55b6e0fbcb5c1904ab0ff4089be32db6da5da
plzusername <MohamedFaris2210@gmail.com>
2024-01-16 20:59:44 +0300

    Add menu section,add domifyobject function

--- src/Utility/objectDomify.js
+++ src/Utility/objectDomify.js
@@ -1,18 +1,18 @@
 import { elemCreator, content } from './elementMaker.js'
 import { menuSection } from '../menu.js'
+
 function convertMenuItemToDom( menuItem ){
 
     let shawermaName = ''
     let ingredients = ''
     let shawermaPrice = ''
 
-    let shawermaItemContainer = elemCreator( 'div', menuSection , `${shawermaName} ${ingredients} ${shawermaPrice} ` , {'class':['shawerm-box']} )
+    let shawermaItemContainer = elemCreator( 'div', menuSection() , `${shawermaName} ${ingredients} ${shawermaPrice} ` , {'class':['shawerm-box']} )
 
     shawermaName = elemCreator( 'h1', shawermaItemContainer , menuItem.shawermaName )
     ingredients = elemCreator( 'p', shawermaItemContainer , menuItem.ingredients )
     shawermaPrice = elemCreator( 'span', shawermaItemContainer , menuItem.price )
 
-    // shawermaItemContainer = elemCreator( 'div', document.body , `${shawermaName} ${ingredients} ${shawermaPrice} ` , {'class':['shawerm-box']} )
 
 }
 
---
+++ src/menu.js
@@ -0,0 +1,52 @@
+import { elemCreator, content } from './Utility/elementMaker.js'
+import { convertMenuItemToDom } from './Utility/objectDomify.js'
+
+class menuItem{
+
+    constructor( shawermaName , ingredients , priceInDollars ) {
+
+        this.shawermaName = shawermaName
+        this.ingredients = ingredients
+        this.price = priceInDollars
+
+    }
+
+    callConvertMenutoDom() {
+        return convertMenuItemToDom(this)
+    }
+
+}
+
+function menuSection(){
+    if(!document.body.contains(document.querySelector('.menu-section'))) {
+
+        return elemCreator( 'div', content , '' , {'class':['menu-section']} )
+
+    }
+
+    else {
+
+        return document.querySelector('.menu-section')
+    }
+}
+
+function menuPage(){
+
+    const menuBoard = elemCreator( '', content , ...[
+        new menuItem('Chicken shawerma','Arabic flat bread, 2lb Chicken gus, 3 Sliced Cucumbers, Finely Diced Tomatoes, Extra Mayonaise with Ketchup, and of course the Special Ingrediant: Motivation!','1.50 JD').callConvertMenutoDom()
+
+        , new menuItem('Meet shawerma','Arabic flat bread, 2lb Meat gus, 1 Sliced cucumber, Spicey red Pepper Sauce, Some ketchonaise, and of course the special ingrediant: Adaptability!','1.50 JD').callConvertMenutoDom()
+
+        , new menuItem('Chicken shawerma Assaj','Arabic flat bread, 2lb Chicken (Assaj), A hot Red Pepper, Spicey red Pepper Sauce, Well cooked Onions, and of course the special ingrediant: Strength!','2.50 JD').callConvertMenutoDom()
+
+        , new menuItem('Meat shawerma Assaj','Arabic flat bread, 2lb Meat (Assaj), A hot Red Pepper, Spicey red Pepper Sauce, Well cooked Onions, and of course the special ingrediant: Tenacity!','2.50 JD').callConvertMenutoDom()
+
+        , new menuItem('Kebab shawerma','Arabic Hamam bread, 3lb Meat Kebab , Extreme amounts of ketchup, Grated cucumbers, Chopped onions and tomatoes, and of course the special ingrediant: Efficiency!','3.00 JD').callConvertMenutoDom()
+
+        , new menuItem('Shish tawook shawerma','Arabic Hamam bread, 3lb Shish tawook (chicken) , Moderate Servings of Matonaise, Grated cucumbers, Chopped onions and tomatoes, and of course the special ingrediant: Efficiency!','3.50 JD').callConvertMenutoDom()
+
+    ] , {'class':['menu-section']} )
+
+}
+
+export {menuPage, menuSection}
