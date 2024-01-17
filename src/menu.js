2f42d4cc8d1672444162d630d03703fda66b850f
plzusername <MohamedFaris2210@gmail.com>
2024-01-17 21:23:00 +0300

    Fix typo's

--- src/menu.js
+++ src/menu.js
@@ -35,7 +35,7 @@ function menuPage(){
     const menuBoard = elemCreator( '', content , ...[
         new menuItem('Chicken shawerma','Arabic flat bread, 2lb Chicken gus, 3 Sliced Cucumbers, Finely Diced Tomatoes, Extra Mayonaise with Ketchup, and of course the Special Ingrediant: Motivation!','1.50 JD').callConvertMenutoDom()
 
-        , new menuItem('Meet shawerma','Arabic flat bread, 2lb Meat gus, 1 Sliced cucumber, Spicey red Pepper Sauce, Some ketchonaise, and of course the special ingrediant: Adaptability!','1.50 JD').callConvertMenutoDom()
+        , new menuItem('Meat shawerma','Arabic flat bread, 2lb Meat gus, 1 Sliced cucumber, Spicey red Pepper Sauce, Some ketchonaise, and of course the special ingrediant: Adaptability!','1.50 JD').callConvertMenutoDom()
 
         , new menuItem('Chicken shawerma Assaj','Arabic flat bread, 2lb Chicken (Assaj), A hot Red Pepper, Spicey red Pepper Sauce, Well cooked Onions, and of course the special ingrediant: Strength!','2.50 JD').callConvertMenutoDom()
 
@@ -43,7 +43,7 @@ function menuPage(){
 
         , new menuItem('Kebab shawerma','Arabic Hamam bread, 3lb Meat Kebab , Extreme amounts of ketchup, Grated cucumbers, Chopped onions and tomatoes, and of course the special ingrediant: Efficiency!','3.00 JD').callConvertMenutoDom()
 
-        , new menuItem('Shish tawook shawerma','Arabic Hamam bread, 3lb Shish tawook (chicken) , Moderate Servings of Matonaise, Grated cucumbers, Chopped onions and tomatoes, and of course the special ingrediant: Resilience!','3.50 JD').callConvertMenutoDom()
+        , new menuItem('Shish tawook shawerma','Arabic Hamam bread, 3lb Shish tawook (chicken) , Moderate Servings of Mayonaise, Grated cucumbers, Chopped onions and tomatoes, and of course the special ingrediant: Resilience!','3.50 JD').callConvertMenutoDom()
 
     ] , {'class':['menu-section']} )
 
