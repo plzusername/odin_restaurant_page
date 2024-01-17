20ada31d039c4d4bcffbc6936b57ecdc7ed487de
plzusername <MohamedFaris2210@gmail.com>
2024-01-17 19:46:54 +0300

    Add main.css in index.js,which includes home.css

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { navBar } from "./nav.js";
-import './style.css'
 import { homePage } from "./home.js";
 import { contactPage } from "./contact.js";
 import { menuPage } from "./menu.js";
 import { footer } from "./footer.js";
+import './styles/main.css'
 
 navBar()
 homePage()
